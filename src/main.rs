7048b45f2d41a4460eef0e49837c956b6c848481
Ivan Shim <zeusmicro@gmail.com>
2025-12-30 07:11:48 +0800

    Refactor main function to readUpdate main to run Lumen programs and evaluate file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod ast;
+mod parser;
+mod eval;
+
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, Lumen!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: lumen <file.lm>");
+        return;
+    }
+    let filename = &args[1];
+    let src = fs::read_to_string(filename).expect("Unable to read file");
+    let stmts = parser::parse_program(&src);
+    eval::eval_program(&stmts);
 }
