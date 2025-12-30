166e5543ac7b790738a8808ae6e98ef22642552f
Ivan Shim <zeusmicro@gmail.com>
2025-12-31 00:53:53 +0800

    Update comparison.rs

--- src/expr/comparison.rs
+++ src/expr/comparison.rs
@@ -1,37 +1,32 @@
-// Comparison operators: == != < > <= >=
-
 use crate::ast::ExprNode;
 use crate::lexer::Token;
 use crate::parser::Parser;
-use crate::registry::{ExprInfix, LumenResult, Precedence};
+use crate::registry::{InfixHandler, LumenResult, Precedence};
 use crate::runtime::{Env, Value};
 
 #[derive(Debug)]
-struct CompareExpr {
-    left: Box<dyn ExprNode>,
+pub struct ComparisonExpr {
     op: Token,
+    left: Box<dyn ExprNode>,
     right: Box<dyn ExprNode>,
 }
 
-impl ExprNode for CompareExpr {
-    fn eval(&self, env: &mut Env) -> LumenResult<Value> {
+impl ExprNode for ComparisonExpr {
+    fn eval(&self, env: &mut Env) -> Result<Value, String> {
         let l = self.left.eval(env)?;
         let r = self.right.eval(env)?;
 
-        use Token::*;
-        use Value::*;
-
-        let result = match (l, r, &self.op) {
-            (Number(a), Number(b), EqEq) => a == b,
-            (Number(a), Number(b), NotEq) => a != b,
-            (Number(a), Number(b), Lt) => a < b,
-            (Number(a), Number(b), Gt) => a > b,
-            (Number(a), Number(b), LtEq) => a <= b,
-            (Number(a), Number(b), GtEq) => a >= b,
+        let result = match (&self.op, l, r) {
+            (Token::EqEq, Value::Number(a), Value::Number(b)) => a == b,
+            (Token::NotEq, Value::Number(a), Value::Number(b)) => a != b,
+            (Token::Lt, Value::Number(a), Value::Number(b)) => a < b,
+            (Token::Gt, Value::Number(a), Value::Number(b)) => a > b,
+            (Token::LtEq, Value::Number(a), Value::Number(b)) => a <= b,
+            (Token::GtEq, Value::Number(a), Value::Number(b)) => a >= b,
             _ => return Err("Invalid comparison".into()),
         };
 
-        Ok(Bool(result))
+        Ok(Value::Bool(result))
     }
 }
 
@@ -45,7 +40,7 @@ impl ComparisonInfix {
     }
 }
 
-impl ExprInfix for ComparisonInfix {
+impl InfixHandler for ComparisonInfix {
     fn matches(&self, parser: &Parser) -> bool {
         parser.peek() == &self.op
     }
@@ -59,8 +54,12 @@ impl ExprInfix for ComparisonInfix {
         parser: &mut Parser,
         left: Box<dyn ExprNode>,
     ) -> LumenResult<Box<dyn ExprNode>> {
-        let op = parser.advance();
-        let right = parser.parse_expr_prec(self.precedence() + 1)?;
-        Ok(Box::new(CompareExpr { left, op, right }))
+        parser.advance();
+        let right = parser.parse_expr_prec(self.precedence())?;
+        Ok(Box::new(ComparisonExpr {
+            op: self.op.clone(),
+            left,
+            right,
+        }))
     }
 }
