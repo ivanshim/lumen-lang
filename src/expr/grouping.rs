b588194f5352af784ff13180c8b6ec29ac8abdf9
Ivan Shim <zeusmicro@gmail.com>
2025-12-31 01:22:27 +0800

    Update grouping.rs

--- src/expr/grouping.rs
+++ src/expr/grouping.rs
@@ -1,18 +1,21 @@
+// Parenthesized expressions: ( ... )
+
 use crate::ast::ExprNode;
 use crate::lexer::Token;
 use crate::parser::Parser;
-use crate::registry::{LumenResult, PrefixHandler};
+use crate::registry::{ExprPrefix, LumenResult};
 
 pub struct GroupingPrefix;
 
-impl PrefixHandler for GroupingPrefix {
+impl ExprPrefix for GroupingPrefix {
     fn matches(&self, parser: &Parser) -> bool {
         matches!(parser.peek(), Token::LParen)
     }
 
     fn parse(&self, parser: &mut Parser) -> LumenResult<Box<dyn ExprNode>> {
-        parser.advance(); // '('
+        parser.advance(); // consume '('
         let expr = parser.parse_expr()?;
+
         match parser.advance() {
             Token::RParen => Ok(expr),
             _ => Err("Expected ')'".into()),
