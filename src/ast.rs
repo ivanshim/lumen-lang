93e8246e3e2b999b170e572e038f6de9a058427f
Ivan Shim <zeusmicro@gmail.com>
2025-12-30 08:34:40 +0800

    Update ast.rs

--- src/ast.rs
+++ src/ast.rs
@@ -2,48 +2,31 @@
 pub enum Expr {
     Number(f64),
     Var(String),
-    Binary {
-        left: Box<Expr>,
-        op: BinOp,
-        right: Box<Expr>,
-    },
-    Compare {
-        left: Box<Expr>,
-        op: CmpOp,
-        right: Box<Expr>,
-    },
+    Binary(Box<Expr>, BinOp, Box<Expr>),
+    Compare(Box<Expr>, CmpOp, Box<Expr>),
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub enum BinOp {
     Add,
     Sub,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub enum CmpOp {
     Eq,
-    Ne,
     Lt,
     Gt,
 }
 
 #[derive(Debug, Clone)]
 pub enum Stmt {
-    Assign {
-        name: String,
-        value: Expr,
-    },
-    While {
-        cond: Expr,
-        body: Vec<Stmt>,
-    },
+    Assign { name: String, value: Expr },
+    While { cond: Expr, body: Vec<Stmt> },
     If {
         cond: Expr,
         then_block: Vec<Stmt>,
         else_block: Option<Vec<Stmt>>,
     },
-    Print {
-        expr: Expr,
-    },
+    Print { expr: Expr },
 }
