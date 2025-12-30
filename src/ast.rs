fb5c91092f37bc7f1e14fc5424c8b3f1d7ae4100
Ivan Shim <zeusmicro@gmail.com>
2025-12-30 15:01:06 +0800

    Update ast.rs

--- src/ast.rs
+++ src/ast.rs
@@ -1,33 +1,109 @@
 #[derive(Debug, Clone)]
-pub enum Expr {
+pub enum Value {
+    None,
+    Bool(bool),
     Number(f64),
-    Var(String),
-    Binary(Box<Expr>, BinOp, Box<Expr>),
-    Compare(Box<Expr>, CmpOp, Box<Expr>),
+    Str(String),
+    List(Vec<Value>),
 }
 
 #[derive(Debug, Clone)]
+pub enum Expr {
+    Literal(Value),
+    Var(String),
+
+    Unary {
+        op: UnOp,
+        expr: Box<Expr>,
+    },
+
+    Binary {
+        left: Box<Expr>,
+        op: BinOp,
+        right: Box<Expr>,
+    },
+
+    Compare {
+        left: Box<Expr>,
+        op: CmpOp,
+        right: Box<Expr>,
+    },
+
+    Call {
+        callee: Box<Expr>,
+        args: Vec<Expr>,
+    },
+
+    Index {
+        base: Box<Expr>,
+        index: Box<Expr>,
+    },
+
+    List(Vec<Expr>),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum UnOp {
+    Neg,
+    Not,
+}
+
+#[derive(Debug, Clone, Copy)]
 pub enum BinOp {
     Add,
     Sub,
+    Mul,
+    Div,
+    And,
+    Or,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 pub enum CmpOp {
     Eq,
-    Lt,
-    Gt,
     Ne,
+    Lt,
+    Le,
+    Gt,
+    Ge,
 }
 
 #[derive(Debug, Clone)]
 pub enum Stmt {
-    Assign { name: String, value: Expr },
-    While { cond: Expr, body: Vec<Stmt> },
+    Assign {
+        name: String,
+        value: Expr,
+    },
+
+    ExprStmt(Expr),
+
+    Print {
+        expr: Expr,
+    },
+
     If {
-        cond: Expr,
-        then_block: Vec<Stmt>,
+        // if/elif/elif/... chain
+        branches: Vec<(Expr, Vec<Stmt>)>,
         else_block: Option<Vec<Stmt>>,
     },
-    Print { expr: Expr },
+
+    While {
+        cond: Expr,
+        body: Vec<Stmt>,
+    },
+
+    ForRange {
+        name: String,
+        start: Expr,
+        end: Expr, // half-open [start, end)
+        body: Vec<Stmt>,
+    },
+
+    FnDef {
+        name: String,
+        params: Vec<String>,
+        body: Vec<Stmt>,
+    },
+
+    Return(Option<Expr>),
 }
