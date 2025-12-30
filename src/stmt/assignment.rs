cee16da97fad73bb62586ca7b80c4d267055b581
Ivan Shim <zeusmicro@gmail.com>
2025-12-31 01:01:27 +0800

    Update assignment.rs

--- src/stmt/assignment.rs
+++ src/stmt/assignment.rs
@@ -1,24 +1,23 @@
 // src/stmt/assignment.rs
 //
-// Variable assignment statement: name = expr
-// Fully removable language feature.
+// assignment:  name = expr
 
-use crate::ast::{Control, ExprNode, StmtNode};
+use crate::ast::{Control, StmtNode};
 use crate::lexer::Token;
 use crate::parser::Parser;
-use crate::registry::{LumenResult, StmtHandler};
+use crate::registry::{err_at, LumenResult, StmtHandler};
 use crate::runtime::Env;
 
 #[derive(Debug)]
 struct AssignStmt {
     name: String,
-    expr: Box<dyn ExprNode>,
+    expr: Box<dyn crate::ast::ExprNode>,
 }
 
 impl StmtNode for AssignStmt {
     fn exec(&self, env: &mut Env) -> LumenResult<Control> {
-        let value = self.expr.eval(env)?;
-        env.set(self.name.clone(), value);
+        let val = self.expr.eval(env)?;
+        env.set(&self.name, val);
         Ok(Control::None)
     }
 }
@@ -27,23 +26,23 @@ pub struct AssignStmtHandler;
 
 impl StmtHandler for AssignStmtHandler {
     fn matches(&self, parser: &Parser) -> bool {
-        matches!(
-            (parser.peek(), parser.peek_n(1)),
-            (Token::Ident(_), Some(Token::Equals))
-        )
+        matches!(parser.peek(), Token::Ident(_))
     }
 
     fn parse(&self, parser: &mut Parser) -> LumenResult<Box<dyn StmtNode>> {
+        // identifier
         let name = match parser.advance() {
             Token::Ident(s) => s,
-            _ => unreachable!(),
+            _ => return Err(err_at(parser, "Expected identifier")),
         };
 
+        // =
         match parser.advance() {
-            Token::Equals => {}
-            _ => return Err(parser.error("Expected '=' in assignment")),
+            Token::Eq => {}
+            _ => return Err(err_at(parser, "Expected '=' in assignment")),
         }
 
+        // expression
         let expr = parser.parse_expr()?;
 
         Ok(Box::new(AssignStmt { name, expr }))
