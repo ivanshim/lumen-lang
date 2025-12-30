e9769f8efd62c34b1cc9707bf5f58b9344e359fa
Ivan Shim <zeusmicro@gmail.com>
2025-12-31 01:22:04 +0800

    Update literals.rs

--- src/expr/literals.rs
+++ src/expr/literals.rs
@@ -1,32 +1,33 @@
+// Number literals
+
 use crate::ast::ExprNode;
 use crate::lexer::Token;
-use crate::registry::{LumenResult, PrefixHandler};
-use crate::runtime::Value;
 use crate::parser::Parser;
+use crate::registry::{ExprPrefix, LumenResult};
+use crate::runtime::{Env, Value};
 
 #[derive(Debug)]
-pub struct NumberLiteral {
+struct NumberLiteral {
     value: f64,
 }
 
 impl ExprNode for NumberLiteral {
-    fn eval(&self, _env: &mut crate::runtime::Env) -> Result<Value, String> {
+    fn eval(&self, _env: &mut Env) -> LumenResult<Value> {
         Ok(Value::Number(self.value))
     }
 }
 
 pub struct NumberLiteralPrefix;
 
-impl PrefixHandler for NumberLiteralPrefix {
+impl ExprPrefix for NumberLiteralPrefix {
     fn matches(&self, parser: &Parser) -> bool {
         matches!(parser.peek(), Token::Number(_))
     }
 
     fn parse(&self, parser: &mut Parser) -> LumenResult<Box<dyn ExprNode>> {
-        if let Token::Number(n) = parser.advance() {
-            Ok(Box::new(NumberLiteral { value: n }))
-        } else {
-            unreachable!()
+        match parser.advance() {
+            Token::Number(n) => Ok(Box::new(NumberLiteral { value: n })),
+            _ => unreachable!(),
         }
     }
 }
