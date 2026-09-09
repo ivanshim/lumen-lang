bfa971419aeba6278743e4bc7ddb67419704d7fb
Ivan Shim <zeusmicro@gmail.com>
2026-09-10 03:53:10 +0800

    Read type defaults apart from module-level nonlocal declarations

--- scratch/syntax-modern/10.py
+++ scratch/syntax-modern/10.py
@@ -1,14 +1,15 @@
 type Pair[K, V] = tuple[K, V]
-def ident[T: (int, str) = int, *Ts, **P](x):
+def ident[T: (int, str), *Ts, **P](x):
     return x
 print(ident(8))
+def defaulted[T = int](x): return x
+print(defaulted(8))
 if False:
     with (
         open("a") as f,
         open("b") as g,
     ):
         print("unreached")
-    nonlocal missing
 print((lambda x: lambda y: y + 1)(0)(3))
 print((lambda x: x if x else 2)(0))
 def enclosing():
