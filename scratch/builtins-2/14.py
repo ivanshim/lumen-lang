3a083fbfdbf01ad252a3af84dbd175841882dd68
Ivan Shim <zeusmicro@gmail.com>
2026-09-10 12:06:05 +0800

    Read scope name lists in a conditional

--- scratch/builtins-2/14.py
+++ scratch/builtins-2/14.py
@@ -1,6 +1,9 @@
 names = sorted({'b': 1, 'a': 2})
 print(names[0], 'a' in names, len(names), names == ['a', 'b'])
-print(bool(sorted({})))
+if sorted({}):
+    print(True)
+else:
+    print(False)
 def empty():
     return dir()
 print(empty())
